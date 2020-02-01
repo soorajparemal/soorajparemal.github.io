4774550dde3de07b7471c7f3dd0e027c3f0c58e6
kenprimo <support@kenprimo.com>
2020-02-01 12:05:07 +0530

    Updates

---
+++ precache-manifest.3763204f2d9232696a5c9684971a2470.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ee5d191a262d6d597fa0d2c8ce895e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b465a65c2e26d6f0df83",
+    "url": "/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "78da706eed78ce9e3123",
+    "url": "/static/js/2.b04a5b54.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b04a5b54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b465a65c2e26d6f0df83",
+    "url": "/static/js/main.c4a1c331.chunk.js"
+  },
+  {
+    "revision": "f920d48f0b4e4ddc4912",
+    "url": "/static/js/runtime-main.59af80b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/eat-app/precache-manifest.262aad56a3a18ca53168c52a47b34214.js"
+  "/precache-manifest.3763204f2d9232696a5c9684971a2470.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eat-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
