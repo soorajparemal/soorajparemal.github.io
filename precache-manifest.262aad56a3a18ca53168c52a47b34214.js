368737660b3e545413773da752da91c510d07572
kenprimo <support@kenprimo.com>
2020-02-01 11:49:33 +0530

    Updates

---
+++ precache-manifest.262aad56a3a18ca53168c52a47b34214.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3c3dc79fcb32d37883a93e3bf4cd95",
+    "url": "/eat-app/index.html"
+  },
+  {
+    "revision": "a7427213347d8a8829ff",
+    "url": "/eat-app/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "78da706eed78ce9e3123",
+    "url": "/eat-app/static/js/2.b04a5b54.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/eat-app/static/js/2.b04a5b54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7427213347d8a8829ff",
+    "url": "/eat-app/static/js/main.5450f488.chunk.js"
+  },
+  {
+    "revision": "0c2612bbbd8173432436",
+    "url": "/eat-app/static/js/runtime-main.3c97498e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/eat-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
